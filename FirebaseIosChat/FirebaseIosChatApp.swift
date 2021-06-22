8846fb1e997a3169bdead9bc2dcd865558aca135
Std1o <37378410+Std1o@users.noreply.github.com>
2021-06-22 20:24:44 +0300

    Initial commit

---
+++ FirebaseIosChat/FirebaseIosChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIosChatApp.swift
+//  FirebaseIosChat
+//
+//  Created by Илья on 22.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseIosChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
